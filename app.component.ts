1f6b131ebb1ae1f4ab1d97a1ce4005177577c3fe
vaibhav131996 <56673880+vaibhav131996@users.noreply.github.com>
2020-03-25 12:44:08 +0530

    form demo

---
+++ app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+ name:string=null;
+ email:string=null;
+ password:string=null;
+ city:string=null;
+ country:string=null;
+ 
+}
